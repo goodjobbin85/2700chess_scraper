5c03b0fc97840e0f137e10c6eb06840564e59faa
Thomas Dubiel <timidturtle85@gmail.com>
2019-07-07 08:18:31 -0500

    add player ratings

--- 2700chess_scraper.rb
+++ 2700chess_scraper.rb
@@ -10,6 +10,14 @@ names = page.css('tr td.name a')
 names.each do |name|
 	puts name.text
 end
+
+ratings = page.css('tr td.live_standard_rating')
+
+name_rating_pair = names.zip(ratings)
+name_rating_pair.each do |name, rating|
+	puts "#{name.text}: #{rating.text}"
+end
+
 puts "#{names.count} chess players"
 #puts "There are #{result.count} chess players on this list"
 
